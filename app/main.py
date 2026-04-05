3d72126642a9acbf32b64be8abc1856d0345e8e6
Blaise Anyigwi <blaiseanyigwi58@gmail.com>
2026-04-04 22:12:40 -0500

    fix cors

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+from fastapi import FastAPI
 
 # ==========================
 # CREATE APP
