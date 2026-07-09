3ece3b4c85036fbdad289590199691ae2c246493
Blaise Anyigwi <blaiseanyigwi58@gmail.com>
2026-07-09 10:22:17 -0500

    Stabilized local development with SQLite

--- app/db/database.py
+++ app/db/database.py
@@ -1,15 +1,34 @@
 import os
 
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
+
+# ----------------------------------------
+# LOAD .ENV
+# ----------------------------------------
+
+load_dotenv()
+
+# ----------------------------------------
+# DATABASE URL
+# ----------------------------------------
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "sqlite:///./aura.db"
 )
 
+print("\n==============================")
+print("DATABASE_URL =", DATABASE_URL)
+print("==============================\n")
+
+# ----------------------------------------
+# ENGINE
+# ----------------------------------------
 
 engine = create_engine(
     DATABASE_URL
